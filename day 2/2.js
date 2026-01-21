c5fb32c6d7493fe1c1203fc8ce4372df97ba0f55
rohit ranjan kumar <rohitranjank25@gmail.com>
2026-01-21 15:42:42 +0530

    commit done

---
+++ day 2/2.js
@@ -0,0 +1,32 @@
+// let sentence ="i am a comma seperated sentence";
+// let words =sentence.split(" ");
+// console.log(words)
+
+// let sentence2 ="i am a comma seperated sentence";
+// let words2 =sentence2.split("a");
+// console.log(words2)
+
+// let str ="tu meri me tera me teri tu mera"
+// let words3=str.split("r").length-1;
+// console.log(words3)
+
+// function demo(){
+//     console.log("demo");
+// }
+// demo()
+
+// function sample(a,b){
+//     return a+b;
+// }
+// console.log(sample(1,10))
+
+// // const test=(c,d)=>c+d;
+// const test=(c,d)=>{
+//     return c+d;
+// };
+// console.log(test(4,0))
+
+(function(){
+    console.log("demo");
+})()
+
