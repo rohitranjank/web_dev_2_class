804ff902d852525f02811fd906c244bfaf433852
rohit ranjan kumar <rohitranjank25@gmail.com>
2026-01-22 11:46:10 +0530

    today commit 22jan

---
+++ day 3/3.js
@@ -0,0 +1,9 @@
+let user ={
+    fullName : "rohit ranjan",
+    address: "delhi",
+    favcolor:["blue","white","green"],
+    mobno:"234564345234"
+    
+}
+// console.log(user)
+console.log(user.fullName,user.favcolor[1],user.address,user.mobno)
