07c3f344e8db5bf69727014805e3708c348eb4c7
rohit ranjan kumar <rohitranjank25@gmail.com>
2026-01-28 15:45:39 +0530

    code done

---
+++ day 5/5.js
@@ -0,0 +1,11 @@
+// const headingElement =document.getElementById('myHeading');
+// console.log(headingElement);
+
+// headingElement.textContent = "new Heading Text";
+// headingElement.style.color = "blue";
+// headingElement.style.backgroundColor ="yellow";
+
+
+const paragraph = document.querySelector("#content p");
+paragraph.textContent ="this paragraph has been updated using querySelector";
+paragraph.style.color="blue";
