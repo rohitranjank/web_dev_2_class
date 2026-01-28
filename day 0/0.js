08e8a6d5a441a8cbdfc97112d745b368ca56d56b
rohit ranjan kumar <rohitranjank25@gmail.com>
2026-01-28 15:49:11 +0530

    code for day 1

---
+++ day 0/0.js
@@ -0,0 +1,28 @@
+// Q1.
+
+// let marks=[32,11,7,24,19,35,10,0,15,17];
+
+// let res = marks.filter((x)=>{
+//     if(x>=16){
+//         console.log(x,"pass")
+//     }
+//     else{
+//         console.log(x,"fail")
+//     }
+// });
+
+// Q2. find the sum of all the elements inside an array with for loop.
+
+// let arr=[1,2,3,4,5]
+// let sum=0;
+// for(let i=0;i<arr.length;i++){
+//     sum=sum+arr[i];
+// }
+// console.log(sum)
+
+// using reduce method
+
+// let arr=[1,2,3,4,5]
+
+// let res = arr.reduce((acc,current)=>acc+current)
+// console.log(res);
