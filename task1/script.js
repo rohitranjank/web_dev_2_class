f24b9e66a91556a6c122974aa3eb3f201c6a7657
rohit ranjan kumar <rohitranjank25@gmail.com>
2026-02-03 12:35:00 +0530

    task456

---
+++ task1/script.js
@@ -0,0 +1,49 @@
+// const profile=document.getElementById('profile');
+
+// profile.style.backgroundColor="pink";
+// profile.style.padding ="15px";
+// profile.style.textAlign ="center";
+
+
+// const x = document.getElementsByClassName("important");
+// console.log(x)
+
+// for(let i = 0 ; i<x.length;i++){
+//     x[i].style.color="red"
+//     x[i].style.fontSize="50px"
+// }
+
+
+// const task3=document.getElementsByTagName("p");
+// console.log(task3);
+
+// for(let i =0 ; i<task3.length;i++){
+//     if (i%2===0){
+//         task3[i].style.color="green"
+//     }
+//     else{
+//         task3[i].style.color="red"
+//     }
+// }
+
+// task3[task3.length-1].style.fontWeight="bold";
+
+
+// const task4=document.querySelectorAll(".box")
+// console.log(task4);
+
+// for(let i =0;i<task4.length;i++){
+
+//     task4[i].textContent="i am first box";
+//     task4[i].style.backgroundColor="green";
+// }
+
+// const section=document.getElementById("content");
+// const task5 =section.querySelectorAll("p");
+// const task5=document.querySelectorAll("#content p");
+
+// for(i=0; i<task5.length;i++){
+//     task5[i].style.color="green";
+// }
+
+const section2=document.getElementById("guest").innerText="Hello World"
