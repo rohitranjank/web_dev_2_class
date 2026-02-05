76e968caf53a9f4e102d2208ab3a8755eb1de4d5
rohit ranjan kumar <rohitranjank25@gmail.com>
2026-02-05 11:55:35 +0530

    button

---
+++ day 7/7.js
@@ -0,0 +1,14 @@
+// function handleClick(){
+//     document.getElementById("first").
+//     textContent="clicked"
+// }
+
+const addText=()=>{
+    document.createElement("h2")
+    textContent="this is an paragraph"
+    const image =document.createElement("img");
+    image.setAttribute("src","https://www.bing.com/images/search?view=detailV2&ccid=nh4rUO6u&id=B73ED52F49BA863C940FCD544614C3E25A80AD1E&thid=OIP.nh4rUO6uSdBUQQMJnEUsmQHaE7&mediaurl=https%3a%2f%2fstatic.vecteezy.com%2fsystem%2fresources%2fpreviews%2f005%2f283%2f061%2foriginal%2fweb-development-concept-in-3d-isometric-design-designer-works-with-code-interface-engineering-programming-settings-and-optimizes-pages-template-with-people-scene-illustration-for-webpage-vector.jpg&exph=2217&expw=3333&q=image+web+dev&FORM=IRPRST&ck=5E5E519E2ABC29B0416AFF42F6B955B3&selectedIndex=0&itb=0")
+
+    document.getElementById("render").append(h2)
+    
+}
